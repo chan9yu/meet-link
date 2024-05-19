69c196ed588284fa0f48e6d6575e7d7a3df320be
chan9yu <wry5533@gmail.com>
2024-05-19 21:54:44 +0900

    refactor: socket events 상수화

--- src/modules/SocketManager.ts
+++ src/modules/SocketManager.ts
@@ -6,10 +6,23 @@ import store from '../store';
 import { setParticipants, setRoomId } from '../store/actions';
 import { rtcManager } from './RTCManager';
 
+enum SocketEvents {
+	CONNECT = 'connect',
+	CONNECTION_INIT = 'connection_init',
+	CONNECTION_PREPARE = 'connection_prepare',
+	CONNECTION_SIGNAL = 'connection_signal',
+	CREATE_ROOM = 'create_room',
+	JOIN_ROOM = 'join_room',
+	ROOM_ID = 'room_id',
+	ROOM_UPDATE = 'room_update',
+	USER_DISCONNECTED = 'user_disconnected'
+}
+
 const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:8080';
 
 class SocketManager {
 	public static instance: SocketManager;
+	public static events = SocketEvents;
 
 	private socket: Socket;
 
@@ -24,44 +37,44 @@ class SocketManager {
 	}
 
 	private initSocketEvents() {
-		this.socket.on('connect', () => {
+		this.socket.on(SocketManager.events.CONNECT, () => {
 			const data = { socketId: this.socket.id };
-			console.log(`<<< successfully connected with socket io server: ${JSON.stringify(data, null, 2)}`);
+			console.log(`successfully connected with socket io server: ${JSON.stringify(data, null, 2)}`);
 		});
 
-		this.socket.on('room_id', data => {
-			console.log(`<<< recv room_id: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.ROOM_ID, data => {
+			console.log(`<<< recv ${SocketManager.events.ROOM_ID}: ${JSON.stringify(data, null, 2)}`);
 			const { roomId } = data;
 			store.dispatch(setRoomId(roomId));
 		});
 
-		this.socket.on('room_update', data => {
-			console.log(`<<< recv room_update: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.ROOM_UPDATE, data => {
+			console.log(`<<< recv ${SocketEvents.ROOM_UPDATE}: ${JSON.stringify(data, null, 2)}`);
 			const { connectedUsers } = data;
 			store.dispatch(setParticipants(connectedUsers));
 		});
 
-		this.socket.on('connection_prepare', data => {
-			console.log(`<<< recv connection_prepare: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.CONNECTION_PREPARE, data => {
+			console.log(`<<< recv ${SocketManager.events.CONNECTION_PREPARE}: ${JSON.stringify(data, null, 2)}`);
 			const { socketId } = data;
 			rtcManager.prepareNewPeerConnection(socketId, false);
 			this.sendConnectionInit(socketId);
 		});
 
-		this.socket.on('connection_signal', data => {
-			console.log(`<<< recv connection_signal: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.CONNECTION_SIGNAL, data => {
+			console.log(`<<< recv ${SocketManager.events.CONNECTION_SIGNAL}: ${JSON.stringify(data, null, 2)}`);
 			const { signal, socketId } = data;
 			rtcManager.handleSignalingData(signal, socketId);
 		});
 
-		this.socket.on('connection_init', data => {
-			console.log(`<<< recv connection_init: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.CONNECTION_INIT, data => {
+			console.log(`<<< recv ${SocketManager.events.CONNECTION_INIT}: ${JSON.stringify(data, null, 2)}`);
 			const { socketId } = data;
 			rtcManager.prepareNewPeerConnection(socketId, true);
 		});
 
-		this.socket.on('user_disconnected', data => {
-			console.log(`<<< recv user_disconnected: ${JSON.stringify(data, null, 2)}`);
+		this.socket.on(SocketManager.events.USER_DISCONNECTED, data => {
+			console.log(`<<< recv ${SocketManager.events.USER_DISCONNECTED}: ${JSON.stringify(data, null, 2)}`);
 			const { socketId } = data;
 			rtcManager.removePeerConnection(socketId);
 		});
@@ -77,26 +90,26 @@ class SocketManager {
 
 	public createRoom(identity: string) {
 		const data = { identity };
-		console.log(`>>> send create_room: ${JSON.stringify(data, null, 2)}`);
-		this.socket.emit('create_room', data);
+		console.log(`>>> send ${SocketManager.events.CREATE_ROOM}: ${JSON.stringify(data, null, 2)}`);
+		this.socket.emit(SocketManager.events.CREATE_ROOM, data);
 	}
 
 	public joinRoom(identity: string, roomId: string) {
 		const data = { identity, roomId };
-		console.log(`>>> send join_room: ${JSON.stringify(data, null, 2)}`);
-		this.socket.emit('join_room', data);
+		console.log(`>>> send ${SocketManager.events.JOIN_ROOM}: ${JSON.stringify(data, null, 2)}`);
+		this.socket.emit(SocketManager.events.JOIN_ROOM, data);
 	}
 
 	public signalPeerData(signal: SignalData, socketId: string) {
 		const data = { signal, socketId };
-		console.log(`>>> send connection_signal: ${JSON.stringify(data, null, 2)}`);
-		this.socket.emit('connection_signal', data);
+		console.log(`>>> send ${SocketManager.events.CONNECTION_SIGNAL}: ${JSON.stringify(data, null, 2)}`);
+		this.socket.emit(SocketManager.events.CONNECTION_SIGNAL, data);
 	}
 
 	public sendConnectionInit(socketId: string) {
 		const data = { socketId };
-		console.log(`>>> send connection_init: ${JSON.stringify(data, null, 2)}`);
-		this.socket.emit('connection_init', data);
+		console.log(`>>> send ${SocketManager.events.CONNECTION_INIT}: ${JSON.stringify(data, null, 2)}`);
+		this.socket.emit(SocketManager.events.CONNECTION_INIT, data);
 	}
 }
 
