d6ec4cc8531217817c16ff6e082a791173796787
chan9yu <wry5533@gmail.com>
2024-05-19 21:41:45 +0900

    chore: vite node-polyfills 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		nodePolyfills({
+			protocolImports: true
+		})
+	],
 	server: {
 		port: 3035
-	},
-	define: {
-		global: {}
 	}
 });
