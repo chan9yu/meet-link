ac231b32f1f72f558d7cb48b2861965c0108489e
chan9yu <wry5533@gmail.com>
2024-06-12 23:23:55 +0900

    refactor: react-hook-form 적용

--- src/components/join_room/JoinRoomContent.tsx
+++ src/components/join_room/JoinRoomContent.tsx
@@ -1,24 +1,34 @@
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '../../constants/router';
 import useRoomConnect from '../../hooks/useRoomConnect';
 import { RootState } from '../../store';
 import { setIdentity } from '../../store/actions';
 import ErrorMessage from './ErrorMessage';
-import JoinRoomButtons from './JoinRoomButtons';
-import JoinRoomInputs from './JoinRoomInputs';
-import OnlyWithAudioCheckbox from './OnlyWithAudioCheckbox';
+import JoinRoomButton from './JoinRoomButton';
+
+type FormValues = {
+	roomId: string;
+	nickname: string;
+};
 
 export default function JoinRoomContent() {
+	const navigate = useNavigate();
+
 	const dispatch = useDispatch();
 	const isRoomHost = useSelector((state: RootState) => state.isRoomHost);
 
-	const [roomId, setRoomId] = useState('');
-	const [nickname, setNickname] = useState('');
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<FormValues>();
 
 	const { createRoom, joinRoom, errorMessage } = useRoomConnect();
 
-	const handleJoinRoom = async () => {
+	const onSubmit: SubmitHandler<FormValues> = async ({ roomId, nickname }) => {
 		dispatch(setIdentity(nickname));
 
 		if (isRoomHost) {
@@ -28,12 +38,33 @@ export default function JoinRoomContent() {
 		}
 	};
 
+	const handleMoveToIntroduction = () => {
+		navigate(RouterPath.INTRODUCTION);
+	};
+
 	return (
-		<>
-			<JoinRoomInputs roomId={roomId} setRoomId={setRoomId} nickname={nickname} setNickname={setNickname} />
-			<OnlyWithAudioCheckbox />
+		<form className="join_room_form" onSubmit={handleSubmit(onSubmit)}>
+			<div className="join_room_inputs_container">
+				{!isRoomHost && (
+					<input
+						className="join_room_input"
+						placeholder="Enter meeting ID"
+						{...register('roomId', { required: 'Meeting ID is required' })}
+					/>
+				)}
+				<input
+					className="join_room_input"
+					placeholder="Enter your Name"
+					{...register('nickname', { required: 'Name is required' })}
+				/>
+			</div>
 			<ErrorMessage errorMessage={errorMessage} />
-			<JoinRoomButtons handleJoinRoom={handleJoinRoom} />
-		</>
+			<div className="join_room_buttons_container">
+				<JoinRoomButton type="submit">{isRoomHost ? 'Host' : 'Join'}</JoinRoomButton>
+				<JoinRoomButton cancelButton onClick={handleMoveToIntroduction}>
+					Cancel
+				</JoinRoomButton>
+			</div>
+		</form>
 	);
 }
