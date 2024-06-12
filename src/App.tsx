4c96007e87c3ec53dd72e8e5ea5f5e4bf28010c6
chan9yu <wry5533@gmail.com>
2024-06-12 23:31:00 +0900

    feat: react-query 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
+
 import Router from './Router';
+import store from './store';
+import './styles/index.css';
+
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+	import('@tanstack/react-query-devtools/build/modern/production.js').then(d => ({
+		default: d.ReactQueryDevtools
+	}))
+);
 
 export default function App() {
-	return <Router />;
+	const [showDevtools, setShowDevtools] = useState(false);
+
+	useEffect(() => {
+		window.toggleDevtools = () => setShowDevtools(old => !old);
+	}, []);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Router />
+			</Provider>
+			<ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
+			{showDevtools && (
+				<Suspense fallback={null}>
+					<ReactQueryDevtoolsProduction />
+				</Suspense>
+			)}
+		</QueryClientProvider>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './store';
-import './styles/index.css';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
+			<App />
 		</React.StrictMode>
 	);
 } else {
