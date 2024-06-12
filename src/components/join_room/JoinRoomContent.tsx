91b2557220b6a68c05cdba8b1b840d31decfaf41
chan9yu <wry5533@gmail.com>
2024-06-12 23:33:28 +0900

    refactor: join-room page 리팩토링

--- src/components/join_room/JoinRoomContent.tsx
+++ src/components/join_room/JoinRoomContent.tsx
@@ -1,11 +1,12 @@
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { useNavigate } from 'react-router-dom';
+
 import { RouterPath } from '../../constants/router';
-import useRoomConnect from '../../hooks/useRoomConnect';
+import { useRoomExistsQuery } from '../../hooks/queries/roomQueries';
 import { RootState } from '../../store';
-import { setIdentity } from '../../store/actions';
+import * as actions from '../../store/actions';
 import ErrorMessage from './ErrorMessage';
 import JoinRoomButton from './JoinRoomButton';
 
@@ -20,16 +21,34 @@ export default function JoinRoomContent() {
 	const dispatch = useDispatch();
 	const isRoomHost = useSelector((state: RootState) => state.isRoomHost);
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors }
-	} = useForm<FormValues>();
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-	const { createRoom, joinRoom, errorMessage } = useRoomConnect();
+	const { register, handleSubmit, getValues } = useForm<FormValues>();
+	const roomId = getValues('roomId') ?? '';
+
+	const { refetch } = useRoomExistsQuery(roomId);
+
+	const createRoom = () => {
+		navigate(RouterPath.ROOM);
+	};
+
+	const joinRoom = async (roomId: string) => {
+		const { data } = await refetch();
+
+		if (!data!.exists) {
+			return setErrorMessage('Meeting not found. Check your meeting id.');
+		}
+
+		if (data!.isFull) {
+			return setErrorMessage('Meeting is full. Please try again later.');
+		}
+
+		dispatch(actions.setRoomId(roomId));
+		navigate(RouterPath.ROOM);
+	};
 
 	const onSubmit: SubmitHandler<FormValues> = async ({ roomId, nickname }) => {
-		dispatch(setIdentity(nickname));
+		dispatch(actions.setIdentity(nickname));
 
 		if (isRoomHost) {
 			createRoom();
@@ -45,18 +64,8 @@ export default function JoinRoomContent() {
 	return (
 		<form className="join_room_form" onSubmit={handleSubmit(onSubmit)}>
 			<div className="join_room_inputs_container">
-				{!isRoomHost && (
-					<input
-						className="join_room_input"
-						placeholder="Enter meeting ID"
-						{...register('roomId', { required: 'Meeting ID is required' })}
-					/>
-				)}
-				<input
-					className="join_room_input"
-					placeholder="Enter your Name"
-					{...register('nickname', { required: 'Name is required' })}
-				/>
+				{!isRoomHost && <input className="join_room_input" placeholder="Enter meeting ID" {...register('roomId')} />}
+				<input className="join_room_input" placeholder="Enter your Name" {...register('nickname')} />
 			</div>
 			<ErrorMessage errorMessage={errorMessage} />
 			<div className="join_room_buttons_container">
